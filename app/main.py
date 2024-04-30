3484963e4f55fdd47c549e0b39d83300f6addf84
Diego <juan.luque@colegiomacaya.cl>
2024-04-29 23:38:36 -0400

    dockerfile chrome 9

--- app/main.py
+++ app/main.py
@@ -114,7 +114,7 @@ async def generate_image_from_html(html_file, output_file, width=None, height=No
     hti = Html2Image()
     with open(html_file, encoding='utf-8') as f:
         html_content = f.read()
-    await hti.screenshot(html_str=html_content, save_as=output_file, size=(width, height))
+    hti.screenshot(html_str=html_content, save_as=output_file, size=(width, height))
     hti.browser = "/usr/bin/google-chrome-stable"
 
 
